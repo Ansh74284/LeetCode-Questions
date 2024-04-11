812b858285818e72167437437741fd690842fa41
Ansh74284 <121850149+Ansh74284@users.noreply.github.com>
2024-04-11 10:49:21 +0530

    Time: 0 ms (100%), Space: 41.4 MB (62.81%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        int n = strs.length;
+
+        String res = strs[0];
+        for(int i=1; i<n; i++){
+            while(strs[i].indexOf(res) != 0){
+                res = res.substring(0,res.length()-1);
+
+                if(res.isEmpty()){
+                    return "";
+                }
+            }
+        }
+        return res;
+    }
+}
